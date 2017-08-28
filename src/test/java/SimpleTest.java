41b1bd01ad415be97fd0426688a424552462c12a
Denis Korbovsky <dkorbovsky@encore.co.uk>
2017-08-28 17:11:07 +0300

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Denis on 28.08.2017.
+ */
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(true);
+    }
+}
